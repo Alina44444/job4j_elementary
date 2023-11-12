80f5d00d1768740f7fc112f5544e5a1c3fd8b3a1
Alina44444 <alina.valiyeva.s@mail.ru>
2023-11-12 18:36:19 +0300

    5.7. Ипотека [#156315]

---
+++ src/main/java/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(double amount, int salary, double percent) {
+        int year = 0;
+        double sum = amount;
+        while (sum > 0) {
+            sum = sum + sum * percent / 100;
+            sum = sum - salary;
+            year++;
+        }
+        return year;
+    }
+}
