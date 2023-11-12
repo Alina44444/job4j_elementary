1ef4fa44a1f9130340994ae64eeb4330c23aec9c
Alina44444 <alina.valiyeva.s@mail.ru>
2023-11-12 13:50:10 +0300

    5.5. Простое число (оператор прерывания break) [#171694 #459369]

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -7,7 +7,7 @@ public class CheckPrimeNumber {
             prime = false;
         }
         for (int i = 2; i < number; i++) {
-            if (i != number && number % i == 0) {
+            if (number % i == 0) {
                 prime = false;
             break;
             }
